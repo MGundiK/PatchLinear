8378cc8771715284b6b7a48162df26da02b7650b
MGundiK <gundic@gmail.com>
2026-03-27 14:56:11 +0100

    Update exp_main.py

--- exp/exp_main.py
+++ exp/exp_main.py
@@ -1,7 +1,7 @@
 from data_provider.data_factory import data_provider
 from exp.exp_basic import Exp_Basic
 from models import xPatch
-from models import GLPatch
+from models import PatchLinear
 from utils.tools import EarlyStopping, adjust_learning_rate, visual
 from utils.metrics import metric
 
